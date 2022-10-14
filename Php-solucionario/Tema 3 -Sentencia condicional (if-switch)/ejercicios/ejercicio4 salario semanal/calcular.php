2097943a9b92bc4ad99cf69d6e28325a42b65cb3
soydangit <soydante34@gmail.com>
2022-10-14 19:47:02 -0400

    asd
    adsfa

---
+++ Php-solucionario/Tema 3 -Sentencia condicional (if-switch)/ejercicios/ejercicio4 salario semanal/calcular.php
@@ -0,0 +1,16 @@
+<!--Vamos a ampliar uno de los ejercicios de la relación anterior para considerar las horas extras. Escribe 
+un programa que calcule el salario semanal de un trabajador teniendo en cuenta que las horas
+ordinarias (40 primeras horas de trabajo)se pagan a 12 euros la hora. A partir de la hora 41, se
+pagan a 16 euros la hora.-->
+<?php
+$nu1=$_POST["n1"];
+if($nu1<=40){
+$primerahora=nu1;
+}
+
+
+echo "<br/> &nbsp; DATOS RECIBIDOS";
+//echo "<br/> el valor de".$40." es: "."3";
+
+
+?>
